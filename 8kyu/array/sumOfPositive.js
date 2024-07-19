7406d29b6d833f901a611d0669486d1aac00571c
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-07-20 00:40:44 +0500

    Time: 799ms

---
+++ 8kyu/array/sumOfPositive.js
@@ -0,0 +1,13 @@
+//8 kyu Sum of positive
+
+/**You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0. */
+
+//solution
+const positiveSum = (arr) =>
+  arr.reduce((sum, curr) => (curr > 0 ? sum + curr : sum), 0);
+
+// Time: 799ms
